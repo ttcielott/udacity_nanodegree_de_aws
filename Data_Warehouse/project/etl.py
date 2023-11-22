47f31c616e41a8b2cbccfeb2248c58553d45804c
Haneul Kim <ttcielott@gmail.com>
2023-11-22 22:25:47 +0000

    added the function to check error message on redshift

--- Data_Warehouse/project/etl.py
+++ Data_Warehouse/project/etl.py
@@ -1,18 +1,32 @@
 import configparser
 import psycopg2
+import pandas as pd
 from sql_queries import copy_table_queries, insert_table_queries
 
 
 def load_staging_tables(cur, conn):
-    for query in copy_table_queries:
+    for query_name, query in copy_table_queries:
         cur.execute(query)
         conn.commit()
+        print(f'{query_name} is DONE')
 
 
 def insert_tables(cur, conn):
-    for query in insert_table_queries:
+    for query_name, query in insert_table_queries:
         cur.execute(query)
         conn.commit()
+        print(f'{query_name} is DONE')
+
+def check_load_error(cur):
+    query = """
+        select  starttime, raw_line, err_reason
+        from stl_load_errors
+        order by starttime desc
+        limit 5;
+    """
+    cur.execute(query)
+    response = cur.fetchall()
+    print(pd.DataFrame(response, columns = ['start_time', 'raw_line', 'error_reason']))
 
 
 def main():
@@ -24,6 +38,7 @@ def main():
     
     load_staging_tables(cur, conn)
     insert_tables(cur, conn)
+    # check_load_error(cur)
 
     conn.close()
 
