146fe59dfaf078baf5423b0998b2720635360415
Haneul Kim <ttcielott@gmail.com>
2023-11-25 11:56:17 +0000

    added function's docstring and extended insert_tables function to show the row number

--- Data_Warehouse/project/etl.py
+++ Data_Warehouse/project/etl.py
@@ -5,6 +5,16 @@ from sql_queries import copy_table_queries, insert_table_queries
 
 
 def load_staging_tables(cur, conn):
+    """
+    The function `load_staging_tables` executes a series of SQL queries to load data into staging tables
+    and prints a message indicating the completion of each query.
+    
+    Args:
+      cur: The "cur" parameter is a cursor object that is used to execute SQL queries and fetch results
+    from the database.
+      conn: The `conn` parameter is a connection object that represents the connection to the database.
+    It is used to establish a connection to the database and execute SQL queries.
+    """
     for query_name, query in copy_table_queries:
         cur.execute(query)
         conn.commit()
@@ -12,12 +22,36 @@ def load_staging_tables(cur, conn):
 
 
 def insert_tables(cur, conn):
-    for query_name, query in insert_table_queries:
-        cur.execute(query)
+    """
+    The function `insert_tables` executes a series of insert queries, commits the changes to the
+    database, and prints the query name and row count.
+    
+    Args:
+      cur: The "cur" parameter is a cursor object that is used to execute SQL queries and fetch results
+    from the database.
+      conn: The `conn` parameter is a connection object that represents the connection to the database.
+    It is used to establish a connection to the database and execute SQL queries.
+    """
+    for query_name, insert_query, check_row_num_query in insert_table_queries:
+        cur.execute(insert_query)
         conn.commit()
         print(f'{query_name} is DONE')
 
+        cur.execute(check_row_num_query)
+        row_num = cur.fetchone()[0]
+        print(f'{query_name} row count: {row_num}')
+
+
 def check_load_error(cur):
+    """
+    The function `check_load_error` retrieves the most recent 5 load errors from the `stl_load_errors`
+    table on Redshift and prints them in a tabular format.
+    
+    Args:
+      cur: The parameter `cur` is a cursor object that is used to execute SQL queries and fetch the
+    results from the database. It is typically obtained by connecting to a database and creating a
+    cursor object using the appropriate database driver.
+    """
     query = """
         select  starttime, raw_line, err_reason
         from stl_load_errors
@@ -30,6 +64,10 @@ def check_load_error(cur):
 
 
 def main():
+    """
+    The main function connects to a Redshift database using the configuration values from a file,
+    loads staging tables, inserts data into tables, and then closes the connection.
+    """
     config = configparser.ConfigParser()
     config.read('dwh.cfg')
 
