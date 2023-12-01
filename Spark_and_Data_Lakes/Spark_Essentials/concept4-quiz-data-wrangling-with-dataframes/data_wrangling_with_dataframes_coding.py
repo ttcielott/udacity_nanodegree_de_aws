f4f6c4b2941fc40e598e8b92a5883a97def0e66d
Haneul Kim <ttcielott@gmail.com>
2023-12-01 18:07:22 +0000

    added codes to answer more quiz questions

--- Spark_and_Data_Lakes/Spark_Essentials/concept4-quiz-data-wrangling-with-dataframes/data_wrangling_with_dataframes_coding.py
+++ Spark_and_Data_Lakes/Spark_Essentials/concept4-quiz-data-wrangling-with-dataframes/data_wrangling_with_dataframes_coding.py
@@ -1,7 +1,7 @@
 # # Data Wrangling with DataFrames Coding Quiz
 
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import udf, sum as Fsum
+from pyspark.sql.functions import udf, sum as Fsum, desc
 from pyspark.sql.window import Window
 from pyspark.sql.types import IntegerType
 
@@ -30,4 +30,56 @@ for row in set(pages_all_user.collect()) - set(pages_non_id_user.collect()):
     print(row.page)
 
 
-# 
+# # Question 2
+#
+# What many female users do we have in the dataset?
+
+# select columns we need
+user_log.select(['userId', 'gender']) \
+        .dropDuplicates() \
+        .groupby('gender') \
+        .count() \
+        .show()
+
+
+# # Question 3
+#
+# How many songs were played played from the most played artist?
+user_log.filter('page = "NextSong"') \
+        .groupby('artist') \
+        .agg({'artist': 'count'}) \
+        .withColumnRenamed('count(Artist)', 'Playcount') \
+        .sort(desc('Playcount')) \
+        .show(1)
+
+# Question 4 (challenge)
+#
+# How many songs do users listen to on average
+# between visiting our homepage?
+# Please round your answer to the closest integer.
+user_window = Window \
+            .partitionBy('userId') \
+            .orderBy(desc('ts')) \
+            .rangeBetween(Window.unboundedPreceding, 0)
+
+isHome= udf(lambda x: int(x == 'Home'), IntegerType())
+
+# filter only NextSong and Home pages, add 1 for each time they visit Home
+# (listening songs -> visiting home)
+# adding a column period which is a specific interval between Home visits
+cusum = user_log.filter((user_log.page == 'NextSong') | (user_log.page == 'Home')) \
+        .select(['userId', 'ts', 'page']) \
+        .withColumn('homevisit', isHome('page')) \
+        .withColumn('period', Fsum('homevisit').over(user_window))
+
+cusum.show(300)
+
+# period : the numbering of home visit
+cusum.filter('page == "NextSong"') \
+     .groupby(['userId', 'period']) \
+     .agg({'period': 'count'}) \
+     .agg({'count(period)': 'avg'}) \
+     .show()
+
+# see how many songs were listened to on average during each period
+
